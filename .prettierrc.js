3686fafcb67c944479bdd22da086e0c759b1d1eb
limingjie <limingjie@chuchujie.com>
2019-06-11 11:19:06 +0800

    增加prettier配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 120, //设置prettier单行输出（不折行）的（最大）长度
+	trailingComma: 'es5', //在任何可能的多行中输入尾逗号
+	useTabs: true, //使用tab（制表位）缩进而非空格
+	tabWidth: 2, //设置工具每一个水平缩进的空格数
+	semi: false, //在语句末尾添加分号
+	singleQuote: true, //使用单引号而非双引号
+	arrowParens: 'avoid', //为单行箭头函数的参数添加圆括号
+	bracketSpacing: true, //在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
+}

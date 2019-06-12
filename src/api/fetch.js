9cce109100115cd46e2403a35d47aa1dbe039c50
limingjie <limingjie@chuchujie.com>
2019-06-12 16:56:14 +0800

    格式化整个项目

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -5,24 +5,24 @@ const API_ROOT = 'http://dev-20190423-meiriyituan.ark-api.daweixinke.com'
 const header = { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }
 
 export default function() {
-	let fetch = {}
-	Object.keys(api).map(key => {
-		return (fetch[key] = function(data = '') {
-			return new Promise((resolve, reject) => {
-				wx.request({
-					url: `${API_ROOT}${api[key][1]}`,
-					data: data,
-					method: api[key][0],
-					header: header,
-					success(res) {
-						resolve(res.data)
-					},
-					fail(err) {
-						reject(err)
-					},
-				})
-			})
-		})
-	})
-	return fetch
+  let fetch = {}
+  Object.keys(api).map(key => {
+    return (fetch[key] = function(data = '') {
+      return new Promise((resolve, reject) => {
+        wx.request({
+          url: `${API_ROOT}${api[key][1]}`,
+          data: data,
+          method: api[key][0],
+          header: header,
+          success(res) {
+            resolve(res.data)
+          },
+          fail(err) {
+            reject(err)
+          },
+        })
+      })
+    })
+  })
+  return fetch
 }
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,17 +1,17 @@
 export default {
-	formatTime: function(date) {
-		const year = date.getFullYear()
-		const month = date.getMonth() + 1
-		const day = date.getDate()
-		const hour = date.getHours()
-		const minute = date.getMinutes()
-		const second = date.getSeconds()
+  formatTime: function(date) {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-		return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-	},
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  },
 
-	formatNumber: function(n) {
-		n = n.toString()
-		return n[1] ? n : '0' + n
-	},
+  formatNumber: function(n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  },
 }
